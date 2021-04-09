5efa5f49c7ecac02014ce8f44e14ae056785d0e3
Skylinker <skylinkertu33@gmail.com>
2021-04-09 13:28:25 +0700

    Added new build site

--- portfolio/photocage/page/assets/js/links.js
+++
@@ -1,8 +0,0 @@
-function nextLinkPage(id, page) {
-	document.getElementById(id).onclick = function () {
-		setTimeout(function () {
-			window.location.href = page;
-		}, 300);
-	};
-}
-nextLinkPage("back", "photocage.html");
--- portfolio/pointjs-help/js/scroll-onwheel.js
+++
@@ -1,8 +0,0 @@
-window.onload = (e) => {
-	document.querySelector('.description-container').style.display = 'none';
-	document.querySelector('.navigation-methods').onmouseover = (e) => {
-		if (e.target.onmouseover ==  typeof 'function') {	
-			console.log(e.target.id);
-		}
-	};
-};
